cab681601f6588cfac1fa4899c3fdf9665273de5
Piero <ironpiero1@gmail.com>
2026-07-23 17:39:59 -0300

    agregado plan model

---
+++ src/models/plan.js
@@ -0,0 +1,27 @@
+var planes =[
+    {
+        id: 1,
+        nombre: "Plan de Entrenamiento para Ciclismo",
+        descripcion: "Este plan de entrenamiento está diseñado para mejorar la resistencia y la fuerza en el ciclismo. Incluye sesiones de entrenamiento en carretera, ejercicios de fuerza y técnicas de recuperación.",
+        deporte:{    
+            id: 1,
+            nombre: "Ciclismo",
+        },
+        fechaInicio: "2024-01-01",
+        fechaFin: "2024-03-31",
+        objetivo: "Mejorar la resistencia y la fuerza en el ciclismo",
+        
+    },{
+        id: 2,
+        nombre: "Plan de Entrenamiento para Natación",
+        descripcion: "Este plan de entrenamiento está diseñado para mejorar la técnica y la velocidad en la natación. Incluye sesiones de entrenamiento en piscina, ejercicios de respiración y técnicas de recuperación.",
+        deporte:{
+            id: 2,
+            nombre: "Natación",
+        },
+        fechaInicio: "2024-02-01",
+        fechaFin: "2024-04-30",
+        objetivo: "Mejorar la técnica y la velocidad en la natación",   
+    }
+]
+export default planes; 
